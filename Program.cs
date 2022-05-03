240064a8249390e0898371cf5f09e3b95afd6390
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-02 21:04:37 -0300

    Identificador de Consoantes e Vogais

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            char a;
+
+            Console.WriteLine("Digite uma letra: ");
+            a = Convert.ToChar(Console.ReadLine());
+
+            if ( 'a' == a || 'e' == a || 'i'== a || 'o'== a || 'u' == a )
+            {
+                Console.WriteLine("Esta é uma vogal");
+            }
+            else
+            {
+                Console.WriteLine("Esta é uma consoante");
+            }
+
+        }
+    }
+}
